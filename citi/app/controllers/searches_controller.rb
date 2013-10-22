c47c46b7a7061df4d966dec0efb9483bcd7cc3d2
My Name <myemail@example.com>
2013-10-21 23:42:20 -0400

    working code on address algorithm

--- citi/app/controllers/searches_controller.rb
+++ citi/app/controllers/searches_controller.rb
@@ -5,7 +5,6 @@ class SearchesController < ApplicationController
 
         @x = @coordinates[0]
         @y = @coordinates[1]
-
  
         @x2 = []
         Citibikenyc.branches.values[2].each do |station_location| 
@@ -22,23 +21,22 @@ class SearchesController < ApplicationController
           @docks <<  dock.values[3]
         end
 
-
-
         @arrays_of_dock_coordinates = @x2.zip(@y2)
 
   
         @distances = []
         @arrays_of_dock_coordinates.each do |lat, long|
         @distances << Math.hypot(@x - lat,@y - long)
+
+        @result = @distances.min
+        i = @distances.index(@result)
+       @closest_dock = @arrays_of_dock_coordinates[i] 
         end
 
-        # @arrays_of_dock_coordinates.each do |lat,long| 
-        #   @result = distance(@x,@y,lat,long)
-        # end
-   
+      @final_glorious_answer = Geocoder.address(@closest_dock)
 
         
-
+        # @available_dock = the lat and long of @result turned into the address. 
 
       render :index
     end
