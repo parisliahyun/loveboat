9f1d74f9ca6151d9bcef0e8ac853a92b425fe0c8
My Name <myemail@example.com>
2013-10-21 23:16:53 -0400

    distance between two points

--- citi/app/controllers/searches_controller.rb
+++ citi/app/controllers/searches_controller.rb
@@ -2,13 +2,11 @@ class SearchesController < ApplicationController
 
   def index
     @coordinates = Geocoder.coordinates(params[:address])
-       
 
-    # this grabs the lat and long from Geocoder for the desired destination.
         @x = @coordinates[0]
         @y = @coordinates[1]
 
-    # # this method calls all the lats and longs for all of the bike docks.
+ 
         @x2 = []
         Citibikenyc.branches.values[2].each do |station_location| 
         @x2 <<  station_location.values[1]
@@ -19,21 +17,28 @@ class SearchesController < ApplicationController
           @y2 <<  station_location.values[2]
         end
 
-        @arrays_of_dock_coordinates = []
-        @arrays_of_dock_coordinates << @x2.zip(@y2)
+        @docks = []
+        Citibikenyc.branches.values[2].each do |dock| 
+          @docks <<  dock.values[3]
+        end
 
-    # setting citibike data equal to an instance variable. 
-    # @cb_ll_list = Citibikenyc.branches.values[2]
 
-    # below measures the distance between the destination and a dock location.
-        # def distance(x,x2,y,y2)
-        # Math.sqrt(( x - x2)**2 + (y - y2)**2)
+
+        @arrays_of_dock_coordinates = @x2.zip(@y2)
+
+  
+        @distances = []
+        @arrays_of_dock_coordinates.each do |lat, long|
+        @distances << Math.hypot(@x - lat,@y - long)
+        end
+
+        # @arrays_of_dock_coordinates.each do |lat,long| 
+        #   @result = distance(@x,@y,lat,long)
         # end
+   
 
-        # @arrays_of_dock_coordinates.each do |lat,long| @result = distance(@x,@y,lat,long)
-        # end
+        
 
-        # @arrays_of_dock_coordinates.min_by do |coordinates| coordinates.distance
 
       render :index
     end
