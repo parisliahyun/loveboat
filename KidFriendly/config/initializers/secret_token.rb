8d5d22bb2875b9b473abfaf97810835faaa78186
My Name <parisliahyun@gmail.com>
2013-12-09 17:17:50 -0500

    first commit kidfriendly

---
+++ KidFriendly/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KidFriendly::Application.config.secret_key_base = 'f5e9b4171946bb98d6e5d3e88dbcb499a0e2b49a2643a8759d70eda1afb08ef2fe5a4b476015bc7603be3e8b55a4071681caf0cf9e5e3cf4699e2f34a7ff729c'
---
+++ KidFriendly/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KidFriendly::Application.config.session_store :cookie_store, key: '_KidFriendly_session'
